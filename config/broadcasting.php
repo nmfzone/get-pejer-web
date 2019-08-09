83709cd32fa0ec458bf85c178459190643a8df90
Nabil Muhammad Firdaus <nabilftd@gmail.com>
2019-08-09 15:56:38 +0700

    chore: Update

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,6 +37,9 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
+                'host' => env('PUSHER_APP_HOST', env('APP_URL')),
+                'port' => env('PUSHER_APP_PORT', 6001),
+                'scheme' => env('PUSHER_APP_SCHEME', 'http'),
                 'useTLS' => true,
             ],
         ],
