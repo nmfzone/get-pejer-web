9332831b719b72a6f6999f05c799ea1cb5cfab60
Nabil Muhammad Firdaus <nabilftd@gmail.com>
2019-10-16 20:59:34 +0700

    chore: Update

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -48,6 +48,5 @@ window.Pusher = Pusher
 
 window.Echo = new Echo({
   broadcaster: 'socket.io',
-  host: process.env.MIX_SOCKET_IO_HOST,
-  channelPrefix: process.env.MIX_SOCKET_IO_CHANNEL_PREFIX
+  host: process.env.MIX_SOCKET_IO_HOST
 })
